7915fedcc61887d41826f31d72325c97dcc7eddb
AnaD <angheloiu.ana.maria@gmail.com>
2024-09-25 16:45:56 +0300

    Update celsius_to_fahrenheit.py

--- celsius_to_fahrenheit.py
+++ celsius_to_fahrenheit.py
@@ -19,24 +19,35 @@ through the function call and the direct calculation.
 """
 
 # Read an integer input from the user, which represents the temperature in Celsius.
-celsius = int(input())
+celsius_input = int(input())
 
 
 # Define a function `conv` that takes a Celsius temperature as an argument.
-def conv(celsius):
-    # Calculate the Fahrenheit equivalent using the conversion formula.
-    # Fahrenheit = (1.8 * Celsius) + 32
-    fahrenheit = (1.8 * celsius) + 32
+def conv(celsius_temp):
+    """
+    Convert a temperature from Celsius to Fahrenheit and print the result.
+
+    Args:
+        celsius (int or float): The temperature in Celsius to be converted.
+
+    Conversion Formula:
+        Fahrenheit = (1.8 * Celsius) + 32
+
+    Prints:
+        The corresponding temperature in Fahrenheit.
+    """
+
+    fahrenheit = (1.8 * celsius_temp) + 32
 
     # Print the calculated Fahrenheit value.
     print(fahrenheit)
 
 
 # Call the `conv` function with the user-provided Celsius value to convert and print it.
-conv(celsius)
+conv(celsius_input)
 
 # Alternatively, directly calculate the Fahrenheit value without using the function.
-fahrenheit = (1.8 * celsius) + 32
+fahrenheit = (1.8 * celsius_input) + 32
 
 # Print the Fahrenheit value calculated outside the function.
 print(fahrenheit)
